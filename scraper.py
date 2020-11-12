6eaaf54ccf17f5109d6d4f32ff6a6763f9699759
Sam <syang0525@gmail.com>
2020-11-12 18:37:35 -0500

    more random files

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,6 +7,15 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 import requests
 import re
+import numpy as np
+from garland import get_content
+
+hashtags = []
+h_set = set()
+with open('hashtags.txt', 'r') as f:
+    for line in f:
+        hashtags.append(line.strip())
+    h_set = set(hashtags)
 
 options = Options()
 options.headless = True
@@ -14,7 +24,6 @@ f = None
 
 def tag_is_english(hashtag):
     if re.match("^[a-zA-Z0-9]+$", hashtag) or re.match("^[a-zA-Z0-9]+$", hashtag[:-1]):
-        print(hashtag)
         return True
     return False
 
@@ -28,36 +37,65 @@ def download_image(url):
         return
 
     src = host_elems[0].find_elements_by_tag_name("img")[0].get_attribute('src')
-
-    with open('images/' + extract_post_id(url) + '.png', 'wb') as f:
+    ext = re.match('.+\.(.{3})\?.+', src).group(1)
+    
+    image_name = 'images/' + extract_post_id(url) + '.' + ext
+    with open(image_name, 'wb') as f:
         f.write(requests.get(src).content)
+    
+    embedding = get_content(image_name)
+    return embedding
 
 def process_post(url):
     global f
-    print(f"processing post at {url}")
     driver.get(url)
     elems = driver.find_elements_by_class_name("xil3i")
-    tags = [ tag.text for tag in elems ]
+    tags = set([ tag.text for tag in elems ])
     # id = extract_post_id(url)
 
-    download_image(url)
-    f.write(f"{url}, {str(tags)}\n")
-    f.flush()
-    print(url, tags)
+    embedding = download_image(url)
+    t = [ 1 if t in tags else 0 for t in hashtags ]
+
+    return [t, embedding]
 
 def do_hashtag(url):
-    print(f"processing hashtag at {url}")
+    print(url)
     driver.get(url)
     WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'eLAPa')))
     tags = driver.find_elements_by_tag_name("a") 
-    print(tags)
     links = [ l.get_attribute('href') for l in tags if '/p/' in l.get_attribute('href') ]
-    print(links)
-    for link in links:
-        process_post(link)
+
+    all_tags = []
+    for link in tqdm(links):
+        all_tags.append(process_post(link))
+    
+    return all_tags
+
+def do_top_hashtags():
+    global f
+
+    hashtag_results = []
+    for hashtag in tqdm(hashtags):
+        hashtag_results.extend(do_hashtag(f"https://instagram.com/explore/tags/{hashtag[1:]}"))
+
+
+    res = []
+    for i in range(len(hashtag_results)):
+        tags = hashtag_results[i][0]
+        embeddings = hashtag_results[i][1]
+        if embeddings is None:
+            continue
+        f.write(f"{i};{tags};[{','.join(map(str, embeddings))}]")
+        for j in range(len(tags)):
+            if tags[j]:
+                res.append([i, j])
+
+    print(res)
+    
+    with open('embeddings.npy', 'wb+') as n:
+        np.save(n, res)
 
 def do_hashtags(url):
-    print(f"processing hashtags at {url}")
     driver.get(url)
     elems = driver.find_elements_by_class_name("_7kTyW")
     links = [ link.get_attribute('href') for link in elems if tag_is_english(link.text) ]
@@ -77,7 +115,7 @@ def do_top_level():
 def main():
     global f
     f = open('out.txt', 'w+')
-    do_top_level()
+    do_top_hashtags()
     f.close()
 
 if __name__ == "__main__":
